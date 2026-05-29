b73da0e9f0ae42acb232220fd4c880d007f23ee9
Asuka Takagi <a.takagi1102@gmail.com>
2026-05-30 00:01:21 +0900

    練習問題①記入済み

--- php-practice_1.php
+++ php-practice_1.php
@@ -59,21 +59,111 @@ foreach ($kanto as $capital) {
 }
 
 // Q8 連想配列-2
+$kanto = [
+    '東京都' => '新宿区',
+    '神奈川県' => '横浜市',
+    '千葉県' => '千葉市',
+    '埼玉県' => 'さいたま市',
+    '栃木県' => '宇都宮市',
+    '群馬県' => '前橋市',
+    '茨城県' => '水戸市'
+];
 
+foreach ($kanto as $prefecture => $capital) {
+   if ($prefecture == '埼玉県') {
+       echo($prefecture . 'の県庁所在地は、' . $capital . 'です。');
+   }
+}
 
 // Q9 連想配列-3
+$kanto = [
+    '東京都' => '新宿区',
+    '神奈川県' => '横浜市',
+    '千葉県' => '千葉市',
+    '埼玉県' => 'さいたま市',
+    '栃木県' => '宇都宮市',
+    '群馬県' => '前橋市',
+    '茨城県' => '水戸市',
+    '愛知県' => '名古屋市',
+    '大阪府' => '大阪市'
+];
 
+foreach ($kanto as $prefecture => $capital) {
+
+    if ($prefecture == '愛知県') {
+        echo $prefecture . 'は関東地方ではありません。' . "\n";
+    } else {
+
+        if ($prefecture == '大阪府') {
+            echo $prefecture . 'は関東地方ではありません。' . "\n";
+        } else {
+            echo $prefecture . 'の県庁所在地は、' . $capital . 'です。' . "\n";
+        }
+
+    }
+}
 
 // Q10 関数-1
+function hello($name)
+{
+    return $name . 'さん、こんにちは。';
+}
+
+echo hello('金谷');
+echo "\n";
+echo hello('安藤');
 
 
 // Q11 関数-2
+function calcTaxInPrice($price)
+{
+    return $price * 1.1;
+}
+
+$price = 1000;
+
+$taxInPrice = calcTaxInPrice($price);
+
+echo $price . '円の商品の税込価格は' . $taxInPrice . '円です。';
 
 
 // Q12 関数とif文
 
+function distinguishNum($num)
+{
+    if ($num % 2 == 0) {
+        return $num . 'は偶数です。';
+    } else {
+        return $num . 'は奇数です。';
+    }
+}
+
+echo distinguishNum(11);
+echo "\n";
+echo distinguishNum(24);
 
 // Q13 関数とswitch文
+function evaluateGrade($grade)
+{
+    switch ($grade) {
 
+        case 'A':
+        case 'B':
+            return '合格です。';
+
+        case 'C':
+            return '合格ですが追加課題があります。';
+
+        case 'D':
+            return '不合格です。';
+
+        default:
+            return '判定不明です。講師に問い合わせてください。';
+    }
+}
+
+echo evaluateGrade('A');
+echo "\n";
+echo evaluateGrade('X');
 
 ?>
